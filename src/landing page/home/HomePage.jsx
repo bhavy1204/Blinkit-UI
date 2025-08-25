0a972a49ea543182445bcc5b20755eb05c4bab37
bhavy1204 <bhavysarwa51@gmail.com>
2025-08-25 09:07:29 +0530

    immplement Home page

--- src/landing page/home/HomePage.jsx
+++ src/landing page/home/HomePage.jsx
@@ -2,7 +2,12 @@ import Categories from "./Categories.jsx"
 import Footer from "../Footer.jsx"
 import Navbar from "../Navbar.jsx"
 import RollingPapers from "./RollingPaper.jsx"
+import MouthFreshner from "./MouthFreshner.jsx"
 import Hero from "./hero.jsx"
+import DairyBread from "./DairyBread.jsx"
+import ColdDrinks from "./ColdDrinks.jsx"
+import Candies from "./Candies.jsx"
+import Hookah from "./Hookah.jsx"
 
 export default function Home() {
     return (
@@ -11,6 +16,11 @@ export default function Home() {
             <Hero/>
             <Categories/>
             <RollingPapers/>
+            <MouthFreshner/>
+            <DairyBread/>
+            <Candies/>
+            <ColdDrinks/>
+            <Hookah/>
             <Footer />
         </>
     )
